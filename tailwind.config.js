6c6b5ea0abfa91825de48d9a2813c13c0cbbf947
dubov <Kirill.Dubov@enterra-inc.com>
2025-04-08 17:46:18 +0300

    feat: add image support and typography plugin to blog post rendering
    - Added remote image patterns for Sanity.io in Next.js config
    - Integrated @tailwindcss/typography plugin for better content styling
    - Implemented PortableTextComponents for image rendering in blog posts
    - Updated blog post page to display main image and body content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import {heroui} from "@heroui/theme"
+import {heroui} from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -20,7 +20,10 @@ const config = {
     },
   },
   darkMode: "class",
-  plugins: [heroui()],
+  plugins: [
+    heroui(),
+    require('@tailwindcss/typography'),
+  ],
 }
 
 module.exports = config;
